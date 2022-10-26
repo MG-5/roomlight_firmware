4a9afd9e635cf0f6e8f5dc790fdd79fb355bd7a1
MG-5 <mg-95@t-online.de>
2022-10-26 03:37:57 +0200

    Check HAL callback registrations at startup

--- src/Application.cxx
+++ src/Application.cxx
@@ -25,32 +25,31 @@ Application::Application()
     HAL_StatusTypeDef result = HAL_OK;
 
     // adc stuff
-    result = HAL_ADC_RegisterCallback(
-        AdcPeripherie, HAL_ADC_CONVERSION_COMPLETE_CB_ID,
-        [](ADC_HandleTypeDef *) //
-        { getApplicationInstance().analogToDigital.conversionCompleteCallback(); });
+    result = HAL_ADC_RegisterCallback(AdcPeripherie, HAL_ADC_CONVERSION_COMPLETE_CB_ID,
+                                      [](ADC_HandleTypeDef *)
+                                      { instance->analogToDigital.conversionCompleteCallback(); });
+    SafeAssert(result == HAL_OK);
 
     // addressable LED stuff
-    result = HAL_DMA_RegisterCallback(
-        DmaLedTimerChannel1, HAL_DMA_XFER_HALFCPLT_CB_ID,
-        [](DMA_HandleTypeDef *) //
-        { getApplicationInstance().addressableLeds.transferHalfHandler(); });
+    result = HAL_DMA_RegisterCallback(DmaLedTimerChannel1, HAL_DMA_XFER_HALFCPLT_CB_ID,
+                                      [](DMA_HandleTypeDef *)
+                                      { instance->addressableLeds.transferHalfHandler(); });
+    SafeAssert(result == HAL_OK);
 
-    result = HAL_DMA_RegisterCallback(
-        DmaLedTimerChannel1, HAL_DMA_XFER_CPLT_CB_ID,
-        [](DMA_HandleTypeDef *) //
-        { getApplicationInstance().addressableLeds.transferCompleteHandler(); });
+    result = HAL_DMA_RegisterCallback(DmaLedTimerChannel1, HAL_DMA_XFER_CPLT_CB_ID,
+                                      [](DMA_HandleTypeDef *)
+                                      { instance->addressableLeds.transferCompleteHandler(); });
+    SafeAssert(result == HAL_OK);
 
     // uart stuff
-    result = HAL_UART_RegisterCallback( //
-        EspUartPeripherie, HAL_UART_TX_COMPLETE_CB_ID,
-        [](UART_HandleTypeDef *) //
-        { getApplicationInstance().uartTx.notifyTxTask(); });
+    result = HAL_UART_RegisterCallback(EspUartPeripherie, HAL_UART_TX_COMPLETE_CB_ID,
+                                       [](UART_HandleTypeDef *) //
+                                       { instance->uartTx.notifyTxTask(); });
+    SafeAssert(result == HAL_OK);
 
     result = HAL_UART_RegisterRxEventCallback(EspUartPeripherie,                     //
                                               [](UART_HandleTypeDef *, uint16_t pos) //
                                               { instance->uartRx.rxEventsFromISR(pos); });
-
     SafeAssert(result == HAL_OK);
 }
 
